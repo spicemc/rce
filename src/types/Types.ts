bc8438b88b94034f6d2b84ee046bfa94774e0906
spicemc <spicemc@users.noreply.github.com>
2023-04-07 12:08:10 +0200

    update/deps Packages updated. TypeScript upgraded to 5.0.3 with @Body decorator fix (#15)

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -13,3 +13,5 @@ export type ClassType = Record<string, unknown>;
 export type Callable = (...args: any[]) => any;
 
 export type DecoratorFunction = (object: any, methodName: string) => void;
+
+export type GenericObject = { [key: string]: any };
