bd475a34f8f83e75d900d409b994bde43a714b8b
spicemc@workstation <andre303@gmx.de>
2023-10-24 17:21:18 +0200

    PUT decorator docs misspelling fixed

--- src/decorator/Put.ts
+++ src/decorator/Put.ts
@@ -9,13 +9,13 @@ import { DecoratorFunction } from '../types/Types';
 export function Put(route?: RegExp, options?: HandlerOptions): DecoratorFunction;
 
 /**
- * Registers an action to be executed when POST request comes on a given route.
+ * Registers an action to be executed when PUT request comes on a given route.
  * Must be applied on a controller action.
  */
 export function Put(route?: string, options?: HandlerOptions): DecoratorFunction;
 
 /**
- * Registers an action to be executed when POST request comes on a given route.
+ * Registers an action to be executed when PUT request comes on a given route.
  * Must be applied on a controller action.
  */
 export function Put(route?: string | RegExp, options?: HandlerOptions): DecoratorFunction {
